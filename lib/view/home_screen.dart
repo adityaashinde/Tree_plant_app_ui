d9930abffacaa291029cdcd411e0dfcfbab8f2cc
adityaashinde <adityashinde4580@gmail.com>
2024-06-22 12:23:28 +0530

    Update home_screen.dart

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,6 +11,52 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: const Color.fromRGBO(251, 247, 248, 1),
+      body: Container(
+        width: double.infinity,
+        child: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 14),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    width: 176,
+                    height: 64,
+                    child: Text(
+                      "Find your favorite plants",
+                      style: GoogleFonts.poppins(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 40,
+                    width: 40,
+                    padding: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Color.fromRGBO(0, 0, 0, 0.15),
+                          offset: Offset(0, 4),
+                          blurRadius: 16.1,
+                        ),
+                      ],
+                    ),
+
+                    ///  child: Image(image: Image.network("src"),),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
