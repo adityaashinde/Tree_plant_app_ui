9e35d9df30bcb37091a956fd4bcd53a57d8d8cd4
adityaashinde <adityashinde4580@gmail.com>
2024-06-30 11:53:30 +0530

    detail_page added

--- lib/view/detail_screen.dart
+++ lib/view/detail_screen.dart
@@ -1,15 +1,344 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:tree_plant_app_ui/view/home_screen.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+class DetailScreen extends StatefulWidget {
+  const DetailScreen({super.key});
 
   @override
-  State createState() => _HomeScreenState();
+  State createState() => _DetailScreenState();
 }
 
-class _HomeScreenState extends State {
+class _DetailScreenState extends State {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: const Color.fromRGBO(251, 247, 248, 1),
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 40,
+            ),
+            Row(
+              children: [
+                const Padding(padding: EdgeInsets.only(left: 20)),
+                SizedBox(
+                  height: 24,
+                  width: 24,
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(context, MaterialPageRoute(
+                        builder: (context) {
+                          return const HomeScreen();
+                        },
+                      ));
+                    },
+                    child: const Icon(Icons.arrow_back),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            SizedBox(
+              height: 243.75,
+              width: 195,
+              child: Image.asset(""),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            SizedBox(
+              height: 8,
+              width: 40,
+              child: Row(
+                children: [
+                  Container(
+                    height: 8,
+                    width: 8,
+                    decoration: const BoxDecoration(
+                      color: Color.fromRGBO(62, 102, 24, 1),
+                      shape: BoxShape.circle,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 7,
+                  ),
+                  Container(
+                    height: 8,
+                    width: 8,
+                    decoration: const BoxDecoration(
+                      color: Color.fromRGBO(197, 214, 181, 1),
+                      shape: BoxShape.circle,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+            Row(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(left: 20),
+                ),
+                SizedBox(
+                  height: 33,
+                  width: 298,
+                  child: Text(
+                    "Plants make your life with minimal and happy love the plants more and enjoy life.",
+                    style: GoogleFonts.poppins(
+                      fontSize: 22,
+                      fontWeight: FontWeight.w600,
+                      color: const Color.fromRGBO(48, 48, 48, 1),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Container(
+              height: 200,
+              width: 355,
+              decoration: BoxDecoration(
+                color: const Color.fromRGBO(118, 152, 75, 1),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 12, top: 18),
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(top: 11),
+                          child: SizedBox(
+                            height: 74,
+                            width: 70,
+                            child: Column(
+                              children: [
+                                Image.asset(
+                                  "name",
+                                  height: 20,
+                                  width: 24,
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                SizedBox(
+                                  height: 20,
+                                  width: 45,
+                                  child: Text(
+                                    "Height",
+                                    style: GoogleFonts.poppins(
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.w500,
+                                      color: const Color.fromRGBO(
+                                          255, 255, 255, 1),
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 15,
+                                  width: 64,
+                                  child: Text(
+                                    "30cm-40cm",
+                                    style: GoogleFonts.poppins(
+                                      fontSize: 10,
+                                      fontWeight: FontWeight.w500,
+                                      color: const Color.fromRGBO(
+                                          255, 255, 255, 1),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 35,
+                        ),
+                        SizedBox(
+                          height: 75,
+                          width: 99,
+                          child: Column(
+                            children: [
+                              Image.asset(
+                                "name",
+                                height: 34,
+                                width: 34,
+                              ),
+                              const SizedBox(
+                                height: 8,
+                              ),
+                              SizedBox(
+                                height: 18,
+                                width: 90,
+                                child: Text(
+                                  "Temperature",
+                                  style: GoogleFonts.poppins(
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.w500,
+                                    color:
+                                        const Color.fromRGBO(255, 255, 255, 1),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                width: 51,
+                                height: 15,
+                                child: Text(
+                                  "20`C-25`C",
+                                  style: GoogleFonts.poppins(
+                                    fontSize: 10,
+                                    fontWeight: FontWeight.w500,
+                                    color: const Color.fromRGBO(
+                                        255, 255, 255, 0.6),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 28,
+                        ),
+                        SizedBox(
+                          height: 75,
+                          width: 99,
+                          child: Column(
+                            children: [
+                              Image.asset(
+                                "name",
+                                height: 34,
+                                width: 34,
+                              ),
+                              const SizedBox(
+                                height: 8,
+                              ),
+                              SizedBox(
+                                height: 18,
+                                width: 25,
+                                child: Text(
+                                  "Pot",
+                                  style: GoogleFonts.poppins(
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.w500,
+                                    color:
+                                        const Color.fromRGBO(255, 255, 255, 1),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 15,
+                                width: 66,
+                                child: Text(
+                                  "Ciramic Pot",
+                                  style: GoogleFonts.poppins(
+                                    fontSize: 10,
+                                    fontWeight: FontWeight.w500,
+                                    color: const Color.fromRGBO(
+                                        255, 255, 255, 0.6),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 27,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: Row(
+                      children: [
+                        Column(
+                          children: [
+                            SizedBox(
+                              height: 25,
+                              width: 85,
+                              child: Text(
+                                "Total Price",
+                                style: GoogleFonts.poppins(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w500,
+                                  color:
+                                      const Color.fromRGBO(255, 255, 255, 0.8),
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 33,
+                              width: 67,
+                              child: Text(
+                                "₹ 350",
+                                style: GoogleFonts.poppins(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w600,
+                                  color: const Color.fromRGBO(255, 255, 255, 1),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          width: 62,
+                        ),
+                        Container(
+                          height: 58,
+                          width: 150,
+                          padding: const EdgeInsets.only(
+                              left: 12.1,
+                              top: 20.16,
+                              right: 12.1,
+                              bottom: 20.16),
+                          decoration: BoxDecoration(
+                            color: const Color.fromRGBO(103, 133, 74, 1),
+                            borderRadius: BorderRadius.circular(8.06),
+                          ),
+                          child: Row(
+                            children: [
+                              Image.asset(
+                                "name",
+                                height: 19.35,
+                                width: 19.35,
+                              ),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              SizedBox(
+                                height: 40,
+                                width: 88,
+                                child: Text(
+                                  "Add to cart",
+                                  style: GoogleFonts.rubik(
+                                    fontSize: 14.52,
+                                    fontWeight: FontWeight.w500,
+                                    color:
+                                        const Color.fromRGBO(255, 255, 255, 1),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -13,48 +13,29 @@ class _HomeScreenState extends State {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromRGBO(251, 247, 248, 1),
-      body: Container(
-        width: double.infinity,
-        child: Column(
-          children: [
-            Container(
-              margin: const EdgeInsets.symmetric(horizontal: 14),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    width: 176,
-                    height: 64,
-                    child: Text(
-                      "Find your favorite plants",
-                      style: GoogleFonts.poppins(
-                        fontSize: 24,
-                        fontWeight: FontWeight.w500,
-                      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 10),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 67,
+                width: 350,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    const Spacer(),
+                    Image.asset(
+                      "name",
                     ),
-                  ),
-                  Container(
-                    height: 40,
-                    width: 40,
-                    padding: const EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(10),
-                      boxShadow: const [
-                        BoxShadow(
-                          color: Color.fromRGBO(0, 0, 0, 0.15),
-                          offset: Offset(0, 4),
-                          blurRadius: 16.1,
-                        ),
-                      ],
-                    ),
-
-                    ///  child: Image(image: Image.network("src"),),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
+              const SizedBox(
+                height: 20,
+              ),
+            ],
+          ),
         ),
       ),
     );
